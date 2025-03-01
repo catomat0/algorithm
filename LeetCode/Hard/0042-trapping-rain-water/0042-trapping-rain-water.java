8b8166b1f05895571a7107fae1115e9c5721d3e1
cat_tomato <catomatot@gmail.com>
2025-03-01 17:23:32 +0900

    Time: 1 ms (64.31%), Space: 46.5 MB (48.84%) - LeetHub

---
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] height) {
+
+        int volume = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+
+        // 투포인터가 겹칠 때 까지
+
+        while (left < right) {
+
+            leftMax = Math.max(height[left], leftMax);
+            rightMax = Math.max(height[right], rightMax);
+
+            if (leftMax <= rightMax) {
+                volume += leftMax - height[left];
+                left++;
+            } else {
+                volume += rightMax - height[right];
+                right--;
+            }
+        }
+        return volume;
+    }
+}

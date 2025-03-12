49a89659d8006a154ca15a4bb085d337997292da
cat_tomato <catomatot@gmail.com>
2025-03-12 13:24:24 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.79 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String[] strings = my_string.split("");
+
+        for (int i = strings.length - 1; i >= 0; i--) {
+            answer += strings[i];
+        }
+        return answer;
+    }
+}

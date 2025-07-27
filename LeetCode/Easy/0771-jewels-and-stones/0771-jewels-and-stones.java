2324b203a177853f38aaa8b4f6e219b5792fa182
cat_tomato <catomatot@gmail.com>
2025-07-27 18:23:30 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones1(String J, String S) {
+
+        int count = 0;
+        Set<Character> freqs = new HashSet<>();
+
+        for (char j : J.toCharArray()) {
+            freqs.add(j);
+        }
+
+        for (char s : S.toCharArray()) {
+            if (freqs.contains(s)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

1369231472c056c18c1da054b061d80df0aca917
김동국 <catomatot@gmail.com>
2025-09-07 18:11:49 +0900

    Time: 54 ms (5.35%), Space: 92.6 MB (95.11%) - LeetHub

---
+++ LeetCode/Medium/0077-combinations/0077-combinations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static void dfs(List<List<Integer>> results, LinkedList<Integer> elements, int n,
+                           int start, int k) {
+        if (k == 0) {
+            results.add(elements.stream().collect(Collectors.toList()));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            elements.add(i);
+            dfs(results, elements, n, i + 1, k - 1);
+            elements.removeLast();
+        }
+
+    }
+
+    public static List<List<Integer>> combine(int n, int k) {
+
+        List<List<Integer>> results = new ArrayList<>();
+        dfs(results, new LinkedList<>(), n, 1, k);
+
+        return results;
+        
+    }
+}

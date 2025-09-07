ca3dd4992cbfa239202ca3a6270dcfa18e480b8c
김동국 <catomatot@gmail.com>
2025-09-07 16:47:55 +0900

    Time: 4 ms (3.79%), Space: 44.8 MB (18.63%) - LeetHub

---
+++ LeetCode/Medium/0046-permutations/0046-permutations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> results = new ArrayList<>();
+        List<Integer> lst = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        dfs(results, new ArrayList<>(), lst);
+
+        return results;
+    }
+
+    public static void dfs(List<List<Integer>> results, List<Integer> prevElements,
+                           List<Integer> elements) {
+
+        if (elements.isEmpty()) {
+            results.add(prevElements.stream().collect(Collectors.toList()));
+        }
+
+        for (Integer e : elements) {
+
+            List<Integer> nextElements = new ArrayList<>(elements);
+            nextElements.remove(e);
+
+            prevElements.add(e);
+            dfs(results, prevElements, nextElements);
+            prevElements.remove(e);
+        }
+    }
+}

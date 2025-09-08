01de0dff26d53bbb2bd378b5264ab9523f1a5a34
김동국 <catomatot@gmail.com>
2025-09-08 14:17:08 +0900

    Time: 92 ms (5.12%), Space: 46.2 MB (11.52%) - LeetHub

---
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,53 @@
+class Solution {
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        Map<Integer, List<Integer>> finishToTakeMap = new HashMap<>();
+
+        for (int[] pre : prerequisites) {
+
+            finishToTakeMap.putIfAbsent(pre[0], new ArrayList<>());
+            finishToTakeMap.get(pre[0]).add(pre[1]);
+        }
+
+        List<Integer> takes = new ArrayList<>();
+        List<Integer> taken = new ArrayList<>();
+
+        for (Integer finish : finishToTakeMap.keySet()) {
+
+            if (!dfs(finishToTakeMap, finish, takes, taken)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean dfs(Map<Integer, List<Integer>> finishToTakeMap, Integer finish,
+                              List<Integer> takes, List<Integer> taken) {
+
+        if (takes.contains(finish)) {
+            return false;
+        }
+
+        if (taken.contains(finish)) {
+            return true;
+        }
+
+        
+        if (finishToTakeMap.containsKey(finish)) {
+
+            takes.add(finish);
+
+            for (Integer take : finishToTakeMap.get(finish)) {
+                if (!dfs(finishToTakeMap, take, takes, taken)) {
+                    return false;
+                }
+            }
+
+            takes.remove(finish);
+            taken.add(finish);
+            
+        }
+
+        return true;
+    }
+}
